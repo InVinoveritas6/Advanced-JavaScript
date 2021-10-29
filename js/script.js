49b651ced979fd0f0b7678241d451261ede86a08
Alexandr Lagodich <sagodich@mail.ru>
2021-10-29 16:47:44 +0300

    Lesson2

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+//...........................................Lesson №2.........................................
+
+// const persone = {
+//    name: "Alex",
+//    tel: '+11111111',
+//    parent: {
+//       mom: 'olga',
+//       dad: 'dad'
+//    }
+// };
+// const clopy = JSON.parse(JSON.stringify(persone));
+
+// clopy.parent.mom = 'natasha';
+
+// console.log(clopy);
+// console.log(persone);
+
+//...........................................Lesson №3.........................................
