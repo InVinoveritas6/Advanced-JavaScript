6ae008fdfe0f72efff0b34bd6b49c902013fcec1
Alexandr Lagodich <sagodich@mail.ru>
2021-11-02 20:22:31 +0300

    Lesson№3

--- js/script.js
+++ js/script.js
@@ -17,3 +17,26 @@
 // console.log(persone);
 
 //...........................................Lesson №3.........................................
+
+// const inputRub = document.querySelector('#rub'),
+//    inputUSD = document.querySelector('#usd');
+
+// inputRub.addEventListener('input', () => {
+//    const request = new XMLHttpRequest();
+
+//    request.open('GET', 'js/currenet.json');
+//    request.setRequestHeader('Content-type', 'application/json', 'charset=utf-8');
+//    request.send();
+
+//    request.addEventListener('readystatechange', () => {
+//       if (request.readyState === 4 && request.status === 200) {
+//          console.log(request.response);
+//          const data = JSON.parse(request.response);
+//          inputUSD.value = (data.current.usd * +inputRub.value).toFixed(2);
+//       } else {
+//          inputUSD.value = "Что-то пошло не так"
+//       }
+//    });
+// });
+
+//...........................................Lesson №4.........................................
